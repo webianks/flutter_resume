bcd6894b85fe4dfc988913af36a9c7d2ee338e36
webianks <webianks@gmail.com>
2019-05-19 19:09:08 +0530

    Added Avatar and intro

--- lib/main.dart
+++ lib/main.dart
@@ -6,31 +6,76 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ramankit Singh',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Container(
-        height: double.infinity,
-        width: double.infinity,
-        color: Colors.black,
-        child: Align(
-          alignment: Alignment.topCenter,
-          child: Container(
-            margin: EdgeInsets.all(40),
-            child: Text(
-              "Ramankit Singh",
-              style: TextStyle(
-                fontFamily: 'GoogleSans',
-                fontSize: 35.0,
-                decoration: TextDecoration.none,
-                color: Colors.white,
-              ),
+        title: 'Ramankit Singh',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Container(
+          height: double.infinity,
+          width: double.infinity,
+          color: Colors.black87,
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              margin: EdgeInsets.all(40),
+              child: Column(children: <Widget>[
+                CircleAvatar(
+                  radius: 70.0,
+                  backgroundImage: AssetImage(
+                    'webianks_dp.jpg',
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: Text(
+                    "Ramankit Singh",
+                    style: TextStyle(
+                      fontFamily: 'RobotoMono',
+                      fontSize: 32.0,
+                      decoration: TextDecoration.none,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Makes Android Apps from Java, Kotlin and Flutter.\nHas been certifed for the same from Google. ",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Raleway',
+                      fontSize: 15.0,
+                      decoration: TextDecoration.none,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: OutlineButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                          side: BorderSide(
+                            color: Colors.white, //Color of the border
+                            style: BorderStyle.solid, //Style of the border
+                            width: 1,
+                          )),
+                        color: Colors.white,
+                      onPressed: () {},
+                      child: Text(
+                        "On the way !",
+                        style: TextStyle(
+                          fontFamily: 'Raleway',
+                          fontSize: 15.0,
+                          color: Colors.purple[300],
+                          decoration: TextDecoration.none,
+                        ),
+                      )),
+                ),
+              ]),
             ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
